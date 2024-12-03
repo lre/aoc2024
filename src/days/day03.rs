cd975ae1bb526bc17b4c58f9a8db81489dc6ea78
Lasse <lasse.reedtz@visma.com>
2024-12-03 10:20:13 +0100

    day3 regex

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,12 +1,35 @@
+use regex::Regex;
 use crate::{Solution, SolutionPair};
-
+use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let mut sol1: u64 = 0;
+    let mut sol2: u64 = 0;
+
+    let s = read_to_string("inputs/day3-1.txt").expect("No file found");
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+
+    let mut mul = true;
+    for cap in re.captures_iter(&s) {
+        match &cap[0] {
+            "do()" => mul = true,
+            "don't()" => mul = false,
+            _ => {
+                let x = &cap[1].parse::<u64>().expect("not a number ");
+                let y = &cap[2].parse::<u64>().expect("not a number ");
+        
+                sol1 += x * y;
+                
+                if mul {
+                    sol2 += x * y;
+                }
+            }
+        }
+
+    }
 
     (Solution::from(sol1), Solution::from(sol2))
 }
